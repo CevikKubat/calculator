f3a4767e9da5e36f00d8b07a3d8f69997eeb059d
Cevik <cev@hotmail.de>
2024-07-30 17:57:29 +0200

    display numbers(as strings) on screen

--- calculator.js
+++ calculator.js
@@ -1,27 +1,84 @@
-const add = function(a, b) {
-  return a + b;
-};
+function main(){
+  const button0 = document.querySelector("#n0");
+  const button1 = document.querySelector("#n1");
+  const button2 = document.querySelector("#n2");
+  const button3 = document.querySelector("#n3");
+  const button4 = document.querySelector("#n4");
+  const button5 = document.querySelector("#n5");
+  const button6 = document.querySelector("#n6");
+  const button7 = document.querySelector("#n7");
+  const button8 = document.querySelector("#n8");
+  const button9 = document.querySelector("#n9");
+  const calculator = document.querySelector("#calculator");
+  const screen = document.querySelector("#screen");
 
-const subtract = function(a, b) {
-	return a - b;
-};
+  calculator.addEventListener("click", (e) => {
+    let target = e.target;
+    const number = target.getAttribute("data-number");
+    console.log(number);
+    if (number !== null){
+      screen.textContent += number;
+    }
+    }
+  )
+}
 
-const sum = function(array) {
-  return array.reduce((acc, curr) => acc + curr, 0);
-};
 
-const multiply = function(array) {
-  return array.reduce((acc, curr) => acc * curr, 1);
-};
 
-const power = function(a, b) {
-	return Math.pow(a, b);
-};
 
-const factorial = function(number) {
-  let sum = 1;
-  for (let i = number; i > 0; i--){
-    sum *= i;
-  }
-  return sum;
-};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const add = function(a, b) {
+  //   return a + b;
+  // };
+  
+  // const subtract = function(a, b) {
+  // 	return a - b;
+  // };
+  
+  // const sum = function(array) {
+  //   return array.reduce((acc, curr) => acc + curr, 0);
+  // };
+  
+  // const multiply = function(array) {
+  //   return array.reduce((acc, curr) => acc * curr, 1);
+  // };
+  
+  // const power = function(a, b) {
+  // 	return Math.pow(a, b);
+  // };
+  
+  // const factorial = function(number) {
+  //   let sum = 1;
+  //   for (let i = number; i > 0; i--){
+  //     sum *= i;
+  //   }
+  //   return sum;
+  // };
+
+
+
+main();
