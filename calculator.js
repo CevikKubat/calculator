0cd21dcf8f8a60cf6412d050995fb9ea337c3460
Cevik <cev@hotmail.de>
2024-08-03 20:51:43 +0200

    handle button input 'C' and '='

--- calculator.js
+++ calculator.js
@@ -1,20 +1,9 @@
 function main(){
-  const button0 = document.querySelector("#n0");
-  const button1 = document.querySelector("#n1");
-  const button2 = document.querySelector("#n2");
-  const button3 = document.querySelector("#n3");
-  const button4 = document.querySelector("#n4");
-  const button5 = document.querySelector("#n5");
-  const button6 = document.querySelector("#n6");
-  const button7 = document.querySelector("#n7");
-  const button8 = document.querySelector("#n8");
-  const button9 = document.querySelector("#n9");
   const calculator = document.querySelector("#calculator");
   const screen = document.querySelector("#screen");
 
   let result = 0;
   let inputString = "";
-  let cleanString = "";
 
   calculator.addEventListener("click", (e) => {
     let target = e.target;
@@ -34,14 +23,17 @@ function main(){
         inputString = cleanInput(inputString);
       }
     }
-    console.log(inputString);
-
+    if (operator == "="){
+      console.log("result");
+      inputString = "";
     }
-
-    
+    if (operator == "C"){
+      inputString = "";
+    }
+    console.log(inputString);
+    }
   )
 }
-
 
 function isValidInput(inputString){
   if (inputString.length >= 2){
@@ -54,12 +46,11 @@ function isValidInput(inputString){
     else if (isOperator(lastChar)) {
       return true;
     }
-
   }
 }
 
 function isOperator(char){
-  if (char === "+" || char === "-" || char === "*" || char === "/" || char === ",") {
+  if (char === "+" || char === "-" || char === "*" || char === "/" || char === ","){
     return true;
   }
 }
@@ -67,8 +58,6 @@ function isOperator(char){
 function cleanInput(inputString){
   return inputString.slice(0, -1);
 }
-
-
 
 
 
