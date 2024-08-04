96b37847c253aaeaa345b716e78a1e0e9ddba2ef
Cevik <cev@hotmail.de>
2024-08-04 11:31:05 +0200

    make calculator work when working with positive numbers

--- calculator.js
+++ calculator.js
@@ -4,6 +4,8 @@ function main(){
 
   let result = 0;
   let inputString = "";
+  let outputString = "";
+  let array = [];
 
   calculator.addEventListener("click", (e) => {
     let target = e.target;
@@ -16,20 +18,36 @@ function main(){
     if (number !== null){
       screen.textContent += number;
       inputString += number;
+      if (checkZeroRepetition(inputString)){
+        inputString = cleanInput(inputString);
+      }
     }
     if (operator !== null){
       inputString += operator;
+      screen.textContent = "";
+      // console.log(isValidInput(inputString))
       if (!isValidInput(inputString)){
         inputString = cleanInput(inputString);
       }
     }
-    if (operator == "="){
-      console.log("result");
-      inputString = "";
+    if (operator === "="){
+      outputString = inputString.replace(/([+\-*\/])/g, ' $1 ');
+      console.log(outputString);
+      let [x, op, y] = outputString.split(' ');
+      // array = Array.from(inputString);
+      // x = array[0];
+      // op = array[1];
+      // y = array[2];
+      console.log(array);
+      result = calculate(x, op, y);
+      screen.textContent = result;
+      inputString = result;
     }
     if (operator == "C"){
       inputString = "";
+      screen.textContent = inputString;
     }
+
     console.log(inputString);
     }
   )
@@ -39,7 +57,8 @@ function isValidInput(inputString){
   if (inputString.length >= 2){
     let lastChar = inputString.slice(-1);
     let secondLastChar = inputString.slice(-2, -1);
-    
+    // console.log(`lastChar |${lastChar}|`)
+    // console.log(`secondLastChar |${secondLastChar}|`)
     if (isOperator(lastChar) && isOperator(secondLastChar)) {
       return false;
     }
@@ -59,48 +78,49 @@ function cleanInput(inputString){
   return inputString.slice(0, -1);
 }
 
+function calculate(x, op, y){
+  x = parseFloat(x);
+  y = parseFloat(y);
+  switch (op) {
+    case "+":
+      return add(x, y);
+    case "-":
+      return subtract(x, y);
+    case "*":
+      return multiply(x, y);
+    case "/":
+      return divide(x, y);
+  }
+}
 
+function checkZeroRepetition(inputString){
+  if (inputString.length >= 2){
+    let firstChar = inputString.slice(0, 1);
+    let secondChar = inputString.slice(1, 2);
+    if (firstChar == "0" && secondChar == "0") {
+      return inputString = "0";
+    }
+  }
+}
 
+function add(x, y) {
+  return x + y;
+}
 
+function subtract(x, y) {
+  return x - y;
+}
 
+function multiply(x, y) {
+  return x * y;
+}
 
-
-
-
-
-
-
-
-
-
-// const add = function(a, b) {
-  //   return a + b;
-  // };
-  
-  // const subtract = function(a, b) {
-  // 	return a - b;
-  // };
-  
-  // const sum = function(array) {
-  //   return array.reduce((acc, curr) => acc + curr, 0);
-  // };
-  
-  // const multiply = function(array) {
-  //   return array.reduce((acc, curr) => acc * curr, 1);
-  // };
-  
-  // const power = function(a, b) {
-  // 	return Math.pow(a, b);
-  // };
-  
-  // const factorial = function(number) {
-  //   let sum = 1;
-  //   for (let i = number; i > 0; i--){
-  //     sum *= i;
-  //   }
-  //   return sum;
-  // };
-
-
+function divide(x, y) {
+  if (y === 0) {
+    return "can't divide by zero";
+  } else {
+    return x / y;
+  }
+}
 
 main();
